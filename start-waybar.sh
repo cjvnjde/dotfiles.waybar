022a4e16c740f7be28900a550e4db705bfeadbf8
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2025-02-02 17:05:08 +0200

    update waybar config

--- start-waybar.sh
+++ start-waybar.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-DE="${WAYBAR_DE:-sway}" 
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -t) DE="$2"; shift 2 ;;
+    *) echo "Usage: $0 -t <desktop_environment>"; exit 1 ;;
+  esac
+done
+
+DE="${DE:-default}"
 CONFIG_FILE="$HOME/.config/waybar/${DE}/config.jsonc"
 
 if [[ ! -f "$CONFIG_FILE" ]]; then
